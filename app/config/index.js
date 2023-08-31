6deea7ea0876974a54233ae5fb0303ef7aa5747c
AlexMaAU <alexma8809@gmail.com>
2023-08-31 13:01:07 +1000

    .env updated

--- app/config/index.js
+++ app/config/index.js
@@ -1,14 +1,14 @@
-const dotenv = require('dotenv')
-dotenv.config()
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+const dotenv = require('dotenv');
+dotenv.config();
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 module.exports = {
-    port: process.env.PORT || 8080,
-    api: {
-        prefix: process.env.API_PREFIX || '/api/v1'
-    },
-    units: process.env.UNITS,
-    key: process.env.KEY,
-    db_connect: process.env.DB_CONNECTION,
-    jwt_key: process.env.JWT_KEY
-}
+  port: process.env.PORT || 8080,
+  api: {
+    prefix: process.env.API_PREFIX || '/api/v1',
+  },
+  units: process.env.UNITS,
+  key: process.env.KEY,
+  db_connect: process.env.DB_CONNECTION,
+  jwt_key: process.env.JWT_KEY,
+};
